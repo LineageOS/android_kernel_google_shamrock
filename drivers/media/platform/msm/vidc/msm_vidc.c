899994f352a4747cc580006747d7bc951b25d721
Suman Mukherjee <sumam@codeaurora.org>
2016-06-15 11:31:04 +0530

    msm: vidc: add ion_handle checking before mapping buffers.
    Compare ion handles in driver instead of matching fds
    to check if a buffer is already mapped or not .
    
    Bug: 28747768
    
    Change-Id: Ifd18d8689351c4a6a22c988d359fb413be19e142
    Signed-off-by: Ashray Kulkarni <ashrayk@codeaurora.org>
    Signed-off-by: Praveen Chavan <pchavan@codeaurora.org>
    Signed-off-by: Deva Ramasubramanian <dramasub@codeaurora.org>
    Signed-off-by: Arun Menon <avmenon@codeaurora.org>
    Signed-off-by: Suman Mukherjee <sumam@codeaurora.org>
    Signed-off-by: Paras Nagda <pnagda@codeaurora.org>
    Signed-off-by: Vasantha Balla <vballa@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_vidc.c
+++ drivers/media/platform/msm/vidc/msm_vidc.c
@@ -260,9 +260,9 @@ struct buffer_info *get_registered_buf(struct msm_vidc_inst *inst,
 	list_for_each_entry(temp, &inst->registeredbufs.list, list) {
 		for (i = 0; (i < temp->num_planes)
 			&& (i < VIDEO_MAX_PLANES); i++) {
-			bool ion_hndl_matches = temp->handle[i] ?
+			bool ion_hndl_matches =
 				msm_smem_compare_buffers(inst->mem_client, fd,
-				temp->handle[i]->smem_priv) : false;
+				temp->handle[i]->smem_priv);
 			if (temp &&
 				(ion_hndl_matches ||
 				(device_addr == temp->device_addr[i])) &&
@@ -272,7 +272,7 @@ struct buffer_info *get_registered_buf(struct msm_vidc_inst *inst,
 				size, temp->buff_off[i])
 				|| OVERLAPS(buff_off, size,
 				temp->buff_off[i],
-				temp->size[i]))) {
+				temp->size[i])) && ion_hndl_matches) {
 					dprintk(VIDC_DBG,
 						"This memory region is already mapped\n");
 					ret = temp;
@@ -472,7 +472,7 @@ int map_and_register_buf(struct msm_vidc_inst *inst, struct v4l2_buffer *b)
 	int i = 0, rc = 0;
 	struct msm_smem *same_fd_handle = NULL;
 	bool check_same_fd_handle = !is_dynamic_output_buffer_mode(b, inst) &&
-		!(inst->session_type == MSM_VIDC_ENCODER &&
+		!( inst->session_type == MSM_VIDC_ENCODER &&
 			b->type == V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE);
 
 	if (!b || !inst) {
@@ -538,6 +538,7 @@ int map_and_register_buf(struct msm_vidc_inst *inst, struct v4l2_buffer *b)
 			goto exit;
 
 		if (check_same_fd_handle)
+		//if (!is_dynamic_output_buffer_mode(b, inst))
 			same_fd_handle = get_same_fd_buffer(
 						&inst->registeredbufs,
 						b->m.planes[i].reserved[0]);
